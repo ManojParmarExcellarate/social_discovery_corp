fa80e2f4391f4c80daad55949592e2f652a0dd80
ManojParmarExcellarate <manoj.parmar@excellarate.com>
2023-03-14 12:12:42 +0530

    changes for infinite scrolling

--- app/services/scraper/facebook/take_profile_full_screenshot.rb
+++ app/services/scraper/facebook/take_profile_full_screenshot.rb
@@ -7,10 +7,17 @@ module Scraper
       end
 
       def perform
-        (1..5).each do |click|    
-          puts "---- Scrolling #{click} ----"
+        screen_height_old = @browser.execute_script("return Math.max(document.body.scrollHeight,document.body.offsetHeight,document.documentElement.clientHeight,document.documentElement.scrollHeight,document.documentElement.offsetHeight);")
+        no_of_scrolls = 0
+        scroll_again = true
+        while scroll_again do
+          no_of_scrolls += 1
+          puts "---- Scrolling #{no_of_scrolls} ----"
           @browser.send_keys :space
-          sleep(2)
+          sleep(3)
+          screen_height_new = @browser.execute_script("return Math.max(document.body.scrollHeight,document.body.offsetHeight,document.documentElement.clientHeight,document.documentElement.scrollHeight,document.documentElement.offsetHeight);")
+          scroll_again = (screen_height_new > screen_height_old && no_of_scrolls >= 100)
+          screen_height_old = screen_height_new
         end
         
         @scrapping_dir = "#{@scrapping_dir}/FullProfile"
