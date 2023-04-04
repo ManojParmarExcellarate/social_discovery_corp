dc5898cb8360c970ce1583dd83d2062a6f668b42
ManojParmarExcellarate <manoj.parmar@excellarate.com>
2023-04-04 11:10:10 +0530

    changes for code

--- app/services/scraper/facebook/facebook_application_service.rb
+++ app/services/scraper/facebook/facebook_application_service.rb
@@ -9,7 +9,15 @@ module Scraper
         }
       }
 
-      OPTIONS = {prefs: PREF, args: %w[--disable-notifications --start-maximized --headless]} #--headless
+      OPTIONS = {
+        prefs: PREF,
+        args: [
+            '--disable-notifications',
+            '--start-maximized',
+            # '--user-data-dir=/home/synerzip/projects/sdc/social_discovery_corp/public/FACEBOOK_SCRAPPING/USER_DATA',
+            '--headless'
+          ]
+        }
     end
   end
 end
--- app/services/scraper/facebook/initiate_scraping.rb
+++ app/services/scraper/facebook/initiate_scraping.rb
@@ -7,9 +7,16 @@ module Scraper
           email: 'souqhfbtesting@gmail.com',
           password: 'Pass@123'
         }
+
+        # @credentials = {
+        #   email: 'manoj.parmar@excellarate.com',
+        #   password: 'SOcialDiscovery@2023'
+        # }
+
         @timestamp = timestamp
         @date_filter = date_filter
         @browser = Watir::Browser.new :chrome, options: OPTIONS
+        # @browser = Watir::Browser.new :firefox #, options: OPTIONS
         @scrapping_dir = "#{Rails.public_path}/FACEBOOK_SCRAPPING/#{@profile_link.split('/').last}-#{@timestamp}"
         Dir.mkdir(@scrapping_dir) unless File.exist?(@scrapping_dir)
       end
@@ -17,7 +24,7 @@ module Scraper
       def perform
         CreateSession.perform(@browser, @credentials)
         @browser.goto(@profile_link)
-        ApplyDateFilter.perform(@browser, @date_filter)
+        # ApplyDateFilter.perform(@browser, @date_filter)
         sleep(3)
         
         TakeProfileFullScreenshot.perform(@browser, @scrapping_dir)
